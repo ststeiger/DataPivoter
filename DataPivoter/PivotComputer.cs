3b0c1dc9f89e36006ee4c5fe81c0f2769956e834
root <quandary82@hailmail.net>
2016-03-18 08:31:23 +0100

    PivotComputer

--- DataPivoter/PivotComputer.cs
+++ DataPivoter/PivotComputer.cs
@@ -4,7 +4,46 @@ namespace DataPivoter
 
 
     public class PivotComputer
-    { }
+    { 
+    
+        public System.Data.DataTable data;
+
+
+
+
+    }
+
+
+    public class cRowGroup
+    {
+        public System.Data.DataTable data;
+
+    }
+
+
+    public class cColumnGroup
+    {
+        public object parent;
+
+
+        public System.Data.DataTable data;
+
+        public System.Collections.Generic.List<string> lsFieldList = new System.Collections.Generic.List<string>();
+
+        public System.Collections.Generic.List<object> lsDistinctValues = new System.Collections.Generic.List<object>();
+
+
+
+    }
+
+
+    public class cDistinctValue
+    {
+        public object Value;
+
+
+    }
+
 
 
 }
